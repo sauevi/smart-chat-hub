ad95f3344e7063fdf685eb8741e5c29fee329b2d
sauvilla <saul.camelo@caseware.com>
2023-11-16 18:14:48 -0500

    feat: add helment and enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,15 +4,19 @@ import * as cookieParser from 'cookie-parser'
 import { ValidationPipe } from '@nestjs/common'
 import * as dotenv from 'dotenv'
 import { JwtAuthGuard } from './guard/jwt-auth.guard'
+import helmet from 'helmet'
 
 dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors()
+
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalGuards(new JwtAuthGuard(new Reflector()))
 
+  app.use(helmet())
   app.use(cookieParser())
 
   await app.listen(8000)
